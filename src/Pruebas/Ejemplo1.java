7ab29934071dfed442a3e763a4f6efc3ad80ff29
Lucia <layestaranga@zubirimanteo.comm>
2021-11-24 09:49:12 +0100

    Cambio el nombre a Marina

--- src/Pruebas/Ejemplo1.java
+++ src/Pruebas/Ejemplo1.java
@@ -3,7 +3,7 @@ package Pruebas;
 public class Ejemplo1 {
 	public static void main(String[] args) {
 		System.out.println("Hola");
-		String nombre="Lucia";
+		String nombre="Marina";
 		System.out.println(nombre);
 	}
 
